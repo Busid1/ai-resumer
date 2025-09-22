445fa2c0533d1862099fef6b3c87199d5d33c8ac
Busid <desarrolloweb214@gmail.com>
2025-09-23 00:48:15 +0200

    fix pdf-worker version

--- src/components/pdf-preview.tsx
+++ src/components/pdf-preview.tsx
@@ -4,7 +4,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import { useState } from "react";
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.mjs';
 
 type PDFPreviewProps = {
   pdfUrl: string;
